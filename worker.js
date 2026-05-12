1e3cd5534552abc9918c25cfc5d13e718e95c7ea
Sg <123773436+sgspec@users.noreply.github.com>
2026-05-13 03:17:16 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -22,7 +22,7 @@ export default {
 
       // 🔥 เรียก Gemini
       const response = await fetch(
-        "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.1-flash-lite-preview:generateContent?key=" + env.GEMINI_KEY,
+        "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.1-flash-lite:generateContent?key=" + env.GEMINI_KEY,
         {
           method: "POST",
           headers: {
